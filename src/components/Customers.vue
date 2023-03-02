ebdf67f81d6184c5d64433a20a20ec3523b14d79
liva <ebammo20@gmail.com>
2023-03-02 13:18:12 +0330

    Remove logs

--- src/components/Customers.vue
+++ src/components/Customers.vue
@@ -5,24 +5,12 @@ import {onUpdated, ref, computed, onMounted} from "vue";
 const {result: customers, loading, error, onResult} = useQuery(filterCustomer, {
   customerName: "cu01"
 });
-const cus = computed(() => {
-  console.log('cu on computed: ', customers.data?.filterCustomer.edges ?? [])
-  return customers.data?.filterCustomer.edges ?? []
-})
-onUpdated(() => {
-  console.log('cu on updated: ', customers)
-})
-onMounted(() => {
-  console.log('cu on mounted: ', customers)
-})
-
+const cus = computed(() => customers.data?.filterCustomer.edges ?? [])
 </script>
 <template>
     <div>
       <input placeholder="مشتری">
       <p>cus: {{cus}}</p>
       <p v-for="cu in cus">{{cu.node.name}}</p>
-<!--      <p>cu01</p>-->
-<!--      <p v-for="cu in cus" >{{cu}}</p>-->
     </div>
 </template>
