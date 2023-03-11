fa28c7aaa9d2fc7884892693e4e87ef54a20f969
liva <ebammo20@gmail.com>
2023-03-11 09:56:03 +0330

    Pass: mock with in component call

--- src/__test__/mock.test.js
+++ src/__test__/mock.test.js
@@ -1,35 +1,51 @@
 import { beforeEach, describe, expect, test, vi } from "vitest";
 import { mockMe } from "../modules/moduleToMock";
 import {callMockMe} from "../modules/callModuleFunction";
+import callMockMeComponent from "../components/callMockMe.vue"
+import {render} from "@testing-library/vue";
 
 describe("mock module", () => {
     beforeEach(() => {
         vi.mock("../modules/moduleToMock");
         vi.clearAllMocks()
     })
-    test('call mockMe', () => {
+    test('mock with in test call', () => {
         mockMe()
         expect(mockMe).toBeCalledTimes(1)
     })
-    test('call mockMe', () => {
+    test('mock with in test call and implementation', () => {
         mockMe.mockImplementation(() => {
-            return 'This is mocked version';
+            return 'This is mocked in test and implementation version';
         })
         const value = mockMe()
         expect(mockMe).toBeCalledTimes(1)
-        expect(value).toBe('This is mocked version')
+        expect(value).toBe('This is mocked in test and implementation version')
     })
     test('mock with in module call', () => {
         callMockMe()
         expect(mockMe).toBeCalledTimes(1)
     })
-    test('call mockMe', () => {
+    test('mock with in module call and implementation', () => {
         mockMe.mockImplementation(() => {
-            return 'This is mocked version';
+            return 'This is mocked in module and implementation version';
         })
         const value = callMockMe()
         expect(mockMe).toBeCalledTimes(1)
-        expect(value).toBe('This is mocked version')
+        expect(value).toBe('This is mocked in module and implementation version')
+    })
+    test('mock with in component call', () => {
+        const component = render(callMockMeComponent)
+        component.getByText('Component mounted.')
+        expect(mockMe).toBeCalledTimes(1)
+    })
+    test('mock with in component call and implementation', () => {
+        mockMe.mockImplementation(() => {
+            return 'This is mocked in component call and implementation';
+        })
+        const component = render(callMockMeComponent)
+        component.getByText('Component mounted.')
+        expect(mockMe).toBeCalledTimes(1)
+        component.getByText('This is mocked in component call and implementation')
     })
 })
 
---
+++ src/components/callMockMe.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {mockMe} from "../modules/moduleToMock";
+</script>
+
+<template>
+<div>
+  <p>Component mounted.</p>
+  <p>{{mockMe()}}</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
