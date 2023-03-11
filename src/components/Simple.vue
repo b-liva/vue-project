a588701d202a0f8ff0cae1e07e1360dc57bb77ec
liva <ebammo20@gmail.com>
2023-03-11 09:01:10 +0330

    Test vue specific

---
+++ src/components/Simple.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from "vue"
+let show = ref(false)
+</script>
+<template>
+    <div>
+        <p>Both</p>
+        <p v-show="show">Show</p>
+        <p v-show="!show">Hide</p>
+    </div>
+</template>
---
+++ src/components/vueSpecific.test.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/vue";
+import { describe, expect, test } from "vitest";
+import Simple from "../components/Simple.vue";
+
+describe("vue specific", () => {
+    test("v-show", () => {
+        const component = render(Simple)
+        component.getByText('Both')
+        const el = component.getByText('Show');
+        console.log("element: ", el);
+        component.getByText('Hide')
+    })
+})
