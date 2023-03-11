20fe5a2b68435f78d48d91fa71d4e46430279c11
liva <ebammo20@gmail.com>
2023-03-11 09:11:59 +0330

    Pass: mock with implementation

--- src/__test__/mock.test.js
+++ src/__test__/mock.test.js
@@ -4,9 +4,18 @@ import { mockMe } from "../modules/moduleToMock";
 describe("mock module", () => {
     beforeEach(() => {
         vi.mock("../modules/moduleToMock");
+        vi.clearAllMocks()
     })
     test('call mockMe', () => {
         mockMe()
         expect(mockMe).toBeCalledTimes(1)
+    })
+    test('call mockMe', () => {
+        mockMe.mockImplementation(() => {
+            return 'This is mocked version';
+        })
+        const value = mockMe()
+        expect(mockMe).toBeCalledTimes(1)
+        expect(value).toBe('This is mocked version')
     })
 })
